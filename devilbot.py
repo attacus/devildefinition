b394fc982e8245a4ea591bcb150fc3aec0fed697
Lilly Ryan <lilly@attacus.net>
2017-11-12 17:30:16 +1100

    Tidying up commented lines

--- devilbot.py
+++ devilbot.py
@@ -29,15 +29,10 @@ def random_definition():
                 print("Duplicate selected. Trying again.")
                 return random_definition()
             else:
-                # print(definition)
+                print(definition)
                 api.update_status(definition)
     except tweepy.TweepError as e:
         print(e.reason)
         sleep(2)
 
-
-
-
-
-# tweet()
 random_definition()
